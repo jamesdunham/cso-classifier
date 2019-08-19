3bf43846303b7f3e18f67d423ad560ce81caac31
James Dunham <james@jdunham.io>
2019-08-19 17:14:20 -0400

    Set output file via argparse

--- cset/apply.py
+++ cset/apply.py
@@ -1,10 +1,16 @@
 """
 Apply the CSO classifier to the result of a BQ query.
 """
+import argparse
+import logging
+import sys
 from datetime import datetime
 
 from cset.batch import Paper, batch_predict
 from cset.bigquery import run_query, flatten_row
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def fetch_papers():
@@ -14,11 +20,15 @@ def fetch_papers():
         yield flatten_row(row)
 
 
-def main():
+def main(output_path):
     papers = (Paper(id=k, **v) for k, v in fetch_papers())
-    output_path = 'CSO Output {}.jsonl'.format(datetime.now().strftime('%Y-%m-%d %X'))
     batch_predict(papers, output_path)
 
 
 if __name__ == '__main__':
-    main()
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    default_output_path = 'CSO Output {}.jsonl'.format(datetime.now().strftime('%Y-%m-%d %X'))
+    parser = argparse.ArgumentParser(usage='Apply the CSO classifier to the result of a BQ query.')
+    parser.add_argument('--output', default=default_output_path)
+    args = parser.parse_args()
+    main(args.output)
